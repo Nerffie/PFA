7ce408cbefda4f1ec35a715e1d04f199fd866c31
Nerffie <44228241+Nerffie@users.noreply.github.com>
2019-05-18 01:38:15 +0100

    First commit

---
+++ src/main/java/com/ecommerce/microcommerce/dao/ProductDao.java
@@ -0,0 +1,20 @@
+package com.ecommerce.microcommerce.dao;
+
+import com.ecommerce.microcommerce.model.Product;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface ProductDao extends JpaRepository<Product, Integer> {
+        List<Product> findAll();
+        Product findById(int id);
+        Product save(Product product);
+        Product deleteById(int id);
+
+    }
+
+
